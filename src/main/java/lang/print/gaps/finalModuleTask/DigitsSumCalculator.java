cc185dd81870520d8e2fffd6ff777a9442a893ef
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-11-21 20:54:10 +0500

    module3 taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first, second, third,fourth;
+        first = number / 1000;
+        second = number % 1000 / 100;
+        third = number % 100 / 10;
+        fourth = number % 10;
+        System.out.println(first + second + third + fourth);
     }
 }
