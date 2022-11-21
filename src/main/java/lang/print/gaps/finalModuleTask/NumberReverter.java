6a7f858128648fe1509ebbbe667a540d7236f1cb
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-11-21 21:03:30 +0500

    module3 taskFinalNew

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -5,6 +5,6 @@ public class NumberReverter {
         int first = number % 10;
         int second = number % 100 / 10;
         int third = number / 100;
-        System.out.println(first +  second + third);
+        System.out.println(first+""+second+""+third);
     }
 }
